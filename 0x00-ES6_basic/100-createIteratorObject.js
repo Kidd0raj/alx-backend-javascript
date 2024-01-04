b899fe001784113ec5e8864b134ce8df1df1c286
rajy <kiddorajy@gmail.com>
2024-01-04 20:08:50 +0000

    code-debug

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,29 +1,7 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        if (currentEmployeeIndex < currentDepartment.length) {
-          const result = {
-            value: currentDepartment[currentEmployeeIndex],
-            done: false,
-          };
-          currentEmployeeIndex++;
-          return result;
-        } else {
-          // Move to the next department
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next(); // Recursive call to handle empty departments
-        }
-      } else {
-        return { done: true };
-      }
-    },
-  };
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-
-  for (const department in reportWithIterator.allEmployees) {
-    result += reportWithIterator.allEmployees[department].join(' | ') + ' | ';
-  }
-
-  // Remove the trailing ' | '
-  return result.slice(0, -3);
+  return reportWithIterator.join(' | ');
 }
